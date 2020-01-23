d96565e49066ce316df512d04dc78b5870d8649e
jbradley311 <jbradley311@gmail.com>
2020-01-23 12:02:24 +0000

    added game logic and associated tests

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,15 +1,15 @@
 public class Board {
-    private int [][] grid = new int[3][3];
+    private int[][] grid = new int[3][3];
 
     public Board() {
 
     }
 
-    public boolean isValidMove(int x, int y){
+    public boolean isValidMove(int x, int y) {
         return isWithinBounds(x, y) && isEmptyCell(x, y);
     }
 
-    public void placeMove (int x, int y, int player) {
+    public void placeMove(int x, int y, int player) {
         grid[x][y] = player;
     }
 
@@ -19,5 +19,22 @@ public class Board {
 
     private boolean isWithinBounds(int x, int y) {
         return x >= 0 && x <= 2 && y >= 0 && y <= 2;
+    }
+
+    private boolean playerHasWon(int player) {
+        for (int i = 0; i < 3; i++) {
+            if (grid[i][0] == player && grid[i][1] == player && grid[i][2] == player) {
+                return true;
+            }
+            if (grid[0][i] == player && grid[1][i] == player && grid[2][i] == player) {
+                return true;
+            }
+        }
+        if (grid[0][0] == player && grid[1][1] == player && grid[2][2] == player) {
+            return true;
+        }
+        if (grid[2][0] == player && grid[1][1] == player && grid[0][2] == player) {
+            return true;
+        }
     }
 }
--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,7 +1,34 @@
 import java.util.Optional;
+import java.util.Scanner;
 
 public class Game {
-    public Optional<Integer> winner() {
-        return Optional.empty();
+
+
+    Board board = new Board();
+
+
+    private final int player1 = 1;
+    private final int player2 = 2;
+    int playerx;
+    int playery;
+    int currentPlayer = player1;
+
+    public static void main(String[] args) {
+        //Scanner scanner = new Scanner(System.in);
+        Game game = new Game();
+    }
+
+    public Game()
+    {
+        do {
+            playerx = nextInt(1);
+            playery = nextInt(2);
+        }while((!board.isValidMove(playerx, playery)));
+        board.placeMove(playerx, playery, currentPlayer);
+
+    }
+
+    private int nextInt(int value){
+        return value;
     }
 }
