24cbe065af148cad10049296525f52b9e178c388
jbradley311 <jbradley311@gmail.com>
2020-02-06 13:30:40 +0000

    creation of new tests on 06_02_20

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,34 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.Scanner;
 
 public class Game {
 
 
-    Board board = new Board();
+  Board board = new Board();
 
 
-    private final int player1 = 1;
-    private final int player2 = 2;
-    int playerx;
-    int playery;
-    int currentPlayer = player1;
+  private final int player1 = 1;
+  private final int player2 = 2;
+  int playerx;
+  int playery;
+  int currentPlayer = player1;
 
-    public static void main(String[] args) {
-        //Scanner scanner = new Scanner(System.in);
-        Game game = new Game();
-    }
+  public static void main(String[] args) {
+    List<Integer> moves = new ArrayList<>();
+    Game game = new Game(moves);
+  }
 
-    public Game()
-    {
-        do {
-            playerx = nextInt(1);
-            playery = nextInt(2);
-        }while((!board.isValidMove(playerx, playery)));
-        board.placeMove(playerx, playery, currentPlayer);
+  public Game(List moves) {
 
-    }
+    do {
+      playerx = getNextMove(moves);
+      playery = getNextMove(moves);
+    } while ((!board.isValidMove(playerx, playery)));
+    board.placeMove(playerx, playery, currentPlayer);
 
-    private int nextInt(int value){
-        return value;
-    }
+  }
+
+  private Integer getNextMove(List<Integer> moves){
+      Integer move = moves.get(0);
+      moves.remove(0);
+      return move;
+
+
+  }
+
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,12 +1,60 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import java.io.InputStreamReader;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 import static org.mockito.Mockito.mock;
 
 class GameTest {
 
+    private Scanner scanner;
+
+    @BeforeEach
+    public void setup() {
+
+        //scanner = mock(Scanner.class);
+    }
+
     @Test
     void gameStartsTest() {
-        new Game();
-        mock(Board.class);
+        List<Integer> moves = new ArrayList<>();
+        moves.add(1);
+        moves.add(1);
+        Game game = new Game(moves);
     }
+
+    @Test
+    void playerChangesAfterPlacingMoveTest() {
+        List<Integer> moves = new ArrayList<>();
+        moves.add(0);
+        moves.add(0);
+        moves.add(1);
+        moves.add(1);
+        moves.add(2);
+        moves.add(2);
+        Game game = new Game(moves);
+
+    }
+
+//    @Test
+//    void gameStartsTest() {
+//        String data = "";
+//        InputStream stdin = System.in;
+//        try {
+//            System.setIn(new ByteArrayInputStream(data.getBytes()));
+//            scanner = new Scanner(System.in);
+//            new Game(scanner);
+//        } finally {
+//            System.setIn(stdin);
+//        }
+//
+//
+//    }
 }
