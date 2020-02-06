20ce65f10dc89c7df8fef0f7a00bc7a6db5f3214
Adrian Potter <a.potter.1@elsevier.com>
2020-02-06 12:54:42 +0000

    Make it possible to add in move data in tests

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,34 +1,26 @@
-import java.util.Optional;
+import java.io.InputStream;
 import java.util.Scanner;
 
 public class Game {
 
+  private final int player1 = 1;
+  private final int player2 = 2;
+  int playerx;
+  int playery;
+  int currentPlayer = player1;
 
+  public static void main(String[] args) {
+    new Game(System.in);
+  }
+
+  public Game(InputStream inputStream) {
+    Scanner scanner = new Scanner(inputStream);
     Board board = new Board();
+    do {
+      playerx = scanner.nextInt();
+      playery = scanner.nextInt();
+    } while ((!board.isValidMove(playerx, playery)));
+    board.placeMove(playerx, playery, currentPlayer);
 
-
-    private final int player1 = 1;
-    private final int player2 = 2;
-    int playerx;
-    int playery;
-    int currentPlayer = player1;
-
-    public static void main(String[] args) {
-        //Scanner scanner = new Scanner(System.in);
-        Game game = new Game();
-    }
-
-    public Game()
-    {
-        do {
-            playerx = nextInt(1);
-            playery = nextInt(2);
-        }while((!board.isValidMove(playerx, playery)));
-        board.placeMove(playerx, playery, currentPlayer);
-
-    }
-
-    private int nextInt(int value){
-        return value;
-    }
+  }
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,12 +1,25 @@
 import org.junit.jupiter.api.Test;
 
-import static org.mockito.Mockito.mock;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.List;
+import java.util.stream.Collectors;
 
 class GameTest {
 
-    @Test
-    void gameStartsTest() {
-        new Game();
-        mock(Board.class);
-    }
+  @Test
+  void gameStartsTest() {
+    List<Integer> coordinates = List.of(1, 1);
+    new Game(getInputStreamFromList(coordinates));
+  }
+
+  private <T> InputStream getInputStreamFromList(List<T> coordinates) {
+    return new ByteArrayInputStream(
+      coordinates
+        .stream()
+        .map(String::valueOf)
+        .collect(Collectors.joining("\n"))
+        .getBytes()
+    );
+  }
 }
