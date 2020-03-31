2f0ee1413476370347a7aad9b2e24f549d808992
Timos_Evans <t.evans@elsevier.com>
2020-03-31 17:38:45 +0300

    Timos

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -2,22 +2,28 @@ import java.util.Scanner;
 
 public class Game {
 
+    private final Player player1 = new Player("Player One", Symbol.X);
+    private final Player player2 = new Player("Player Two", Symbol.O);
+    private Player currentPlayer = player1;
+
     public Game(){
-
     }
 
-    private void playGame(){
-        Player player1 = new Player("Player One", Symbol.X);
-        Player player2 = new Player("Player Two", Symbol.O);
-        Player currentPlayer = player1;
+    public void play(){
+
         System.out.println(player1.getName() + " Please Enter Input: ");
+        int playerMove = takeUserInput();
+        switchCurrentPlayer();
 
     }
 
-
-    private int takeUserInput(){
+    public int takeUserInput(){
         Scanner scanner = new Scanner(System.in);
         return scanner.nextInt();
+    }
+
+    public void switchCurrentPlayer(){
+        currentPlayer = currentPlayer == player1 ? player2 : player1;
     }
 
 }
---
+++ src/test/java/GameTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class GameTest {
+
+    @BeforeEach
+    private void Setup(){
+        Game game = new Game();
+    }
+
+    @Test
+
+}
