dedbbb084312e76c394eaf7dbb628a146e92c66e
jbradley311 <jbradley311@gmail.com>
2020-04-21 16:30:37 +0100

    John 21/04

--- src/main/java/GameRunner.java
+++ src/main/java/GameRunner.java
@@ -2,25 +2,26 @@ import java.io.InputStream;
 
 public class GameRunner {
 
-    private Game game;
+  private Game game;
 
-    public GameRunner(){
-        this.game = new Game();
+  public GameRunner() {
+    this.game = new Game();
+  }
+
+  public GameRunner(Game game) {
+    this.game = game;
+  }
+
+  public void runGame(InputStream inputStream) {
+
+    while (!game.isGameDraw()) {
+      int userInput = game.takeUserInput(inputStream);
+      game.placeUserMove(userInput);
+      if (game.isGameWon()) {
+        break;
+      }
+      game.switchCurrentPlayer();
     }
 
-    public GameRunner(Game game){
-        this.game = game;
-    }
-
-    public void runGame(InputStream inputStream){
-
-        while (!game.isGameDraw()) {
-            int userInput = game.takeUserInput(inputStream);
-            game.placeUserMove(userInput);
-            if (game.isGameWon()){
-                break;
-            }
-        }
-
-    }
+  }
 }
--- src/test/java/GameRunnerTest.java
+++ src/test/java/GameRunnerTest.java
@@ -1,21 +1,35 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.io.ByteArrayInputStream;
 
 public class GameRunnerTest {
 
-    GameRunner gameRunner;
+  GameRunner gameRunner;
 
-    @BeforeEach
-    void setup(){
-        gameRunner = new GameRunner();
-    }
+  @BeforeEach
+  void setup() {
+    gameRunner = new GameRunner();
+  }
 
-    @Test
-    void shouldRunGame(){
-        String mockUserInput = "0";
-        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(mockUserInput.getBytes());
-        gameRunner.runGame(byteArrayInputStream);
-    }
+  @Test
+  void shouldRunGame() {
+    String mockUserInput = "0";
+    ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(mockUserInput.getBytes());
+    gameRunner.runGame(byteArrayInputStream);
+  }
+
+  @Test
+  void gameEndsWhenWinner() {
+    String mockUserInput = "0";
+    Game game = Mockito.mock(Game.class);
+    Mockito.when(game.isGameDraw()).thenReturn(false);
+    Mockito.when(game.isGameWon()).thenReturn(true);
+    GameRunner winGameRunner = new GameRunner(game);
+    ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(mockUserInput.getBytes());
+    winGameRunner.runGame(byteArrayInputStream);
+  }
+
 }
