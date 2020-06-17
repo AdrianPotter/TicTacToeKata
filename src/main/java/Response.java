448183c7d4ae87dc1b2d3912352878491b5b6643
Hughes <owainarwynhughes@gmail.com>
2020-06-17 09:18:25 +0100

    Backing UP

---
+++ src/main/java/Response.java
@@ -0,0 +1,14 @@
+public class Response {
+
+    String response;
+    Grid grid;
+
+    public Response(Grid grid){
+        this.grid = grid;
+    }
+
+    private void constructResponse(){
+
+    }
+
+}
