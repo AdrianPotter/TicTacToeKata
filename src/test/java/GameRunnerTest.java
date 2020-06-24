9385bf47b60e853893cd30f98cb9d1bc3b3664bf
Adrian Potter <a.potter.1@elsevier.com>
2020-06-24 15:44:24 +0100

    Change test name to reflect action

--- src/test/java/GameRunnerTest.java
+++ src/test/java/GameRunnerTest.java
@@ -2,12 +2,10 @@ import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class GameRunnerTest {
 
     @Test
-    void userInputIsReturned() {
+    void canRunGameWithOneMove() {
         Game game = new Game();
         String mockInput = "0";
         ByteArrayInputStream mockInputStream = new ByteArrayInputStream(mockInput.getBytes());
