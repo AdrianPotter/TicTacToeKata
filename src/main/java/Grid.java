8b1dde0aa067d13a95d4c76478041edbd0ea896f
Adrian Potter <a.potter.1@elsevier.com>
2020-06-24 16:28:48 +0100

    Apply IntelliJ advised improvements

--- src/main/java/Grid.java
+++ src/main/java/Grid.java
@@ -1,29 +1,29 @@
 import java.util.ArrayList;
 
 public class Grid {
-    ArrayList<Player> grid = new ArrayList<Player>();
-    private final int GRIDSIZE = 9;
+    ArrayList<Player> grid = new ArrayList<>();
+    private static final int GRID_SIZE = 9;
 
-    public Grid(){
-        for(int i = 0; i < GRIDSIZE; i++){
+    public Grid() {
+        for (int i = 0; i < GRID_SIZE; i++) {
             grid.add(null);
         }
     }
 
 
-    public Player getCell(int index){
+    public Player getCell(int index) {
         return grid.get(index);
     }
 
-    public void setCell(int index, Player player){
+    public void setCell(int index, Player player) {
         grid.set(index, player);
     }
 
-    public Integer getGridSize(){
+    public Integer getGridSize() {
         return grid.size();
     }
 
-    public boolean hasEmptyCell(){
+    public boolean hasEmptyCell() {
         return grid.contains(null);
     }
 }
