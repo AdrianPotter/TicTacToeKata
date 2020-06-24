38a465fc05afc4f4e66680be5007fe11095ed92f
Adrian Potter <a.potter.1@elsevier.com>
2020-06-24 15:45:29 +0100

    Use mock over hard dependency

--- src/test/java/GameRunnerTest.java
+++ src/test/java/GameRunnerTest.java
@@ -2,11 +2,13 @@ import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 
+import static org.mockito.Mockito.mock;
+
 public class GameRunnerTest {
 
     @Test
     void canRunGameWithOneMove() {
-        Game game = new Game();
+        Game game = mock(Game.class);
         String mockInput = "0";
         ByteArrayInputStream mockInputStream = new ByteArrayInputStream(mockInput.getBytes());
 
