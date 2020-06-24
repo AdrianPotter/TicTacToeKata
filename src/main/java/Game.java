ccf3b0c1402a2a7b158ee0742e581f3158abd5b5
Adrian Potter <a.potter.1@elsevier.com>
2020-06-24 16:03:05 +0100

    Use named variables over magic numbers

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,4 +1,15 @@
 public class Game {
+
+    private static final Integer TOP_LEFT = 0;
+    private static final Integer TOP_CENTER = 1;
+    private static final Integer TOP_RIGHT = 2;
+    private static final Integer MIDDLE_LEFT = 3;
+    private static final Integer MIDDLE_CENTER = 4;
+    private static final Integer MIDDLE_RIGHT = 5;
+    private static final Integer BOTTOM_LEFT = 6;
+    private static final Integer BOTTOM_CENTER = 7;
+    private static final Integer BOTTOM_RIGHT = 8;
+
     private final Player player1;
     private final Player player2;
     private Player currentPlayer;
@@ -37,19 +48,26 @@ public class Game {
     }
 
     public boolean currentPlayerHasWon() {
-        return horizontalWinningConditionMet() || verticalWinningConditionMet() || diagonalWinningConditionMet();
+        return horizontalWinningConditionMet()
+            || verticalWinningConditionMet()
+            || diagonalWinningConditionMet();
     }
 
     private boolean verticalWinningConditionMet() {
-        return cellsMatch(0, 3, 6) || cellsMatch(1, 4, 7) || cellsMatch(2, 5, 8);
+        return cellsMatch(TOP_LEFT, MIDDLE_LEFT, BOTTOM_LEFT)
+            || cellsMatch(TOP_CENTER, MIDDLE_CENTER, BOTTOM_CENTER)
+            || cellsMatch(TOP_RIGHT, MIDDLE_RIGHT, BOTTOM_RIGHT);
     }
 
     private boolean horizontalWinningConditionMet() {
-        return cellsMatch(0, 1, 2) || cellsMatch(3, 4, 5) || cellsMatch(6, 7, 8);
+        return cellsMatch(TOP_LEFT, TOP_CENTER, TOP_RIGHT)
+            || cellsMatch(MIDDLE_LEFT, MIDDLE_CENTER, MIDDLE_RIGHT)
+            || cellsMatch(BOTTOM_LEFT, BOTTOM_CENTER, BOTTOM_RIGHT);
     }
 
     private boolean diagonalWinningConditionMet() {
-        return cellsMatch(0, 4, 8) || cellsMatch(2, 4, 6);
+        return cellsMatch(TOP_LEFT, MIDDLE_CENTER, BOTTOM_RIGHT)
+            || cellsMatch(TOP_RIGHT, MIDDLE_CENTER, BOTTOM_LEFT);
     }
 
     public boolean gameIsADraw() {
