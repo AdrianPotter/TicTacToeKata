eb6de5b4413638a205167e6ec38809335fd77962
Adrian Potter <a.potter.1@elsevier.com>
2020-06-24 15:47:04 +0100

    Run auto formatting

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -4,63 +4,63 @@ public class Game {
     private Player currentPlayer;
     private Grid grid;
 
-    public Game(){
+    public Game() {
         player1 = new Player();
         player2 = new Player();
         currentPlayer = player1;
         grid = new Grid(); //TODO: Hard dependency on Grid
     }
 
-    public void switchPlayer(){
+    public void switchPlayer() {
         currentPlayer = currentPlayer == player1 ? player2 : player1;
     }
 
-    public void placeMove(int index) throws IllegalArgumentException{
-        if(!isValidMove(index)){
+    public void placeMove(int index) throws IllegalArgumentException {
+        if (!isValidMove(index)) {
             throw new IllegalArgumentException("Invalid Move");
-        }else{
+        } else {
             grid.setCell(index, currentPlayer);
         }
 
     }
 
-    public boolean isValidMove(int index){
-        return(isMoveWithinGrid(index) && hasMoveAlreadyBeenPlayed(index));
+    public boolean isValidMove(int index) {
+        return (isMoveWithinGrid(index) && hasMoveAlreadyBeenPlayed(index));
     }
 
-    private boolean isMoveWithinGrid(int index){
-        return(index >= 0 && index < grid.getGridSize());
+    private boolean isMoveWithinGrid(int index) {
+        return (index >= 0 && index < grid.getGridSize());
     }
 
-    private boolean hasMoveAlreadyBeenPlayed(int index){
-        return(grid.getCell(index) == null);
+    private boolean hasMoveAlreadyBeenPlayed(int index) {
+        return (grid.getCell(index) == null);
     }
 
-    public boolean currentPlayerHasWon(){
+    public boolean currentPlayerHasWon() {
         return (horizontalWinningConditionMet() || verticalWinningConditionMet() || diagonalWinningConditionMet());
     }
 
-    private boolean verticalWinningConditionMet(){
+    private boolean verticalWinningConditionMet() {
         return (cellsMatch(0, 3, 6) || cellsMatch(1, 4, 7) || cellsMatch(2, 5, 8));
     }
 
-    private boolean horizontalWinningConditionMet(){
+    private boolean horizontalWinningConditionMet() {
         return (cellsMatch(0, 1, 2) || cellsMatch(3, 4, 5) || cellsMatch(6, 7, 8));
     }
 
-    private boolean diagonalWinningConditionMet(){
+    private boolean diagonalWinningConditionMet() {
         return (cellsMatch(0, 4, 8) || cellsMatch(2, 4, 6));
     }
 
-    public boolean gameIsADraw(){
-        return(!grid.hasEmptyCell());
+    public boolean gameIsADraw() {
+        return (!grid.hasEmptyCell());
     }
 
-    private boolean cellsMatch(int index1, int index2, int index3){
-        return(grid.getCell(index1) == currentPlayer && grid.getCell(index2) == currentPlayer && grid.getCell(index3) == currentPlayer);
+    private boolean cellsMatch(int index1, int index2, int index3) {
+        return (grid.getCell(index1) == currentPlayer && grid.getCell(index2) == currentPlayer && grid.getCell(index3) == currentPlayer);
     }
 
-    public Player getCurrentPlayer(){
+    public Player getCurrentPlayer() {
         return currentPlayer;
     }
 
